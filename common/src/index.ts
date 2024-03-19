d668ab0265b9ed9308628d173d22bd12c1afe336
Ayush <ayushpanchal005@gmail.com>
2024-03-19 16:51:30 +0530

    added zod validation and types in common directory, also published it to npm

---
+++ common/src/index.ts
@@ -0,0 +1,27 @@
+import z from 'zod';
+
+export const signupInput = z.object({
+  email: z.string().email(),
+  password: z.string(),
+  name: z.string().optional(),
+});
+
+export const signinInput = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export const createPostInput = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+export const updatePostInput = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+export type SignupType = z.infer<typeof signupInput>;
+export type SigninType = z.infer<typeof signinInput>;
+export type CreatePostInput = z.infer<typeof createPostInput>;
+export type UpdatePostInput = z.infer<typeof updatePostInput>;
