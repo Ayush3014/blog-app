d870cd53e4f7404816203766a58f42977ab21dd3
Ayush <ayushpanchal005@gmail.com>
2024-03-19 14:41:27 +0530

    added the middleware to verify jwt to ensure user login and fixed env variables not initialized error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 import { Hono } from 'hono';
-import { sign } from 'hono/jwt';
+import { sign, verify } from 'hono/jwt';
 
 // passing database url type as a generic to access it in context (c)
 const app = new Hono<{
@@ -9,11 +9,14 @@ const app = new Hono<{
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
+  Variables: {
+    userId: string;
+  };
 }>();
 
 app.post('/api/v1/user/signup', async (c) => {
   const prisma = new PrismaClient({
-    datasourceUrl: c.env?.DATABASE_URL,
+    datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
   const body = await c.req.json();
@@ -56,19 +59,45 @@ app.post('/api/v1/user/signin', async (c) => {
   return c.json({ jwt });
 });
 
-app.post('/api/v1/blog', (c) => {
-  return c.text('Blog');
-});
-
-app.put('/api/v1/blog', (c) => {
-  return c.text('Update blog');
-});
-
 app.get('/api/v1/blog/bulk', (c) => {
   return c.text('All blogs');
 });
 
+// middleware verifying the token
+app.use('/api/v1/blog/*', async (c, next) => {
+  const jwt = c.req.header('Authorization');
+  if (!jwt) {
+    c.status(401);
+    return c.json({ error: 'Unauthorized' });
+  }
+
+  const token = jwt.split(' ')[1];
+  const payload = await verify(token, c.env.JWT_SECRET);
+  if (!payload) {
+    c.status(401);
+    return c.json({ error: 'Unauthorized' });
+  }
+
+  c.set('userId', payload.id);
+
+  await next();
+});
+
+// add middleware here, user posts a blog
+app.post('/api/v1/blog', (c) => {
+  console.log(c.get('userId'));
+  return c.text('Blog');
+});
+
+// add middleware here, user updates a blog
+app.put('/api/v1/blog', (c) => {
+  console.log(c.get('userId'));
+  return c.text('Update blog');
+});
+
+// add middleware here, user gets a blog with a specific id
 app.get('/api/v1/blog/:id', (c) => {
+  console.log(c.get('userId'));
   const id = c.req.param('id');
   return c.text('Blog with id: ' + id);
 });
