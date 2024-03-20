9ae3e691f7f5c13d784553dfa399c11b703c9b8d
Ayush <ayushpanchal005@gmail.com>
2024-03-20 18:35:00 +0530

    added custom hook to fetch the blogs from db and made the blogs component dynamic

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
+
 interface BlogCardType {
   title: string;
   content: string;
   authorName: string;
   publishedDate: string;
+  id: string;
 }
 
 export function BlogCard({
@@ -10,29 +13,32 @@ export function BlogCard({
   content,
   authorName,
   publishedDate,
+  id,
 }: BlogCardType) {
   return (
-    <div className="p-4 border-b border-slate-200 pb-4">
-      <div className="flex">
-        <Avatar name={authorName} size="small" />
+    <Link to={`/blog/${id}`}>
+      <div className="p-4 border-b border-slate-200 pb-4 w-screen max-w-screen-md">
+        <div className="flex">
+          <Avatar name={authorName} size="small" />
 
-        <div className="font-extralight pl-2 text-sm flex justify-center flex-col">
-          {authorName}
+          <div className="font-extralight pl-2 text-sm flex justify-center flex-col">
+            {authorName}
+          </div>
+          <div className="flex flex-col justify-center pl-2">
+            <Circle />
+          </div>
+          <div className="pl-2 font-thin text-slate-500 flex flex-col justify-center text-sm">
+            {publishedDate}
+          </div>
         </div>
-        <div className="flex flex-col justify-center pl-2">
-          <Circle />
-        </div>
-        <div className="pl-2 font-thin text-slate-500 flex flex-col justify-center text-sm">
-          {publishedDate}
-        </div>
+
+        <div className="text-xl font-semibold pt-2">{title}</div>
+        <div className="text-md font-thin">{content.slice(0, 100) + '...'}</div>
+        <div className="text-sm text-slate-500 font-thin pt-4">{`${Math.ceil(
+          content.length / 100
+        )} minute(s) read`}</div>
       </div>
-
-      <div className="text-xl font-semibold pt-2">{title}</div>
-      <div className="text-md font-thin">{content.slice(0, 100) + '...'}</div>
-      <div className="text-sm text-slate-500 font-thin pt-4">{`${Math.ceil(
-        content.length / 100
-      )} minute(s) read`}</div>
-    </div>
+    </Link>
   );
 }
 
